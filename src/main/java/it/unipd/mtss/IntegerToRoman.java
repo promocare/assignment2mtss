9eaacd69c72070f50796fd008e101c9e59b24979
Giulio Moretto - Promocare <giulio@promocare.it>
2023-05-11 10:25:34 +0200

    close #8
    Aggiunta funzionalità di conversione e test per i numeri da 7 a 10

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -11,6 +11,15 @@ public class IntegerToRoman {
         
         String romanNumeral = "";
 
+        while (number >= 10) {
+            romanNumeral += "X";
+            number -= 10;
+        }
+        while (number >= 9) {
+            romanNumeral += "IX";
+            number -= 9;
+        }
+
         while(number >=1){
             romanNumeral += "I";
             number -= 1;
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -14,33 +14,85 @@ public class IntegerToRomanTest
      * Test the number 1
      */
     @Test
-    public void testNumberOne()
+    public void testNumberOneConversion()
     {
         String result = IntegerToRoman.convert(1);
 
-        assertEquals(result, "I");
+        assertEquals("I",result);
     }
 
     /**
      * Test the number 2
      */
     @Test
-    public void testNumberTwo()
+    public void testNumberTwoConversion()
     {
         String result = IntegerToRoman.convert(2);
 
-        assertEquals(result, "II");
+        assertEquals("II",result);
     }
 
     /**
      * Test the number 3
      */
     @Test
-    public void testNumberThree()
+    public void testNumberThreeConversion()
     {
 
         String result = IntegerToRoman.convert(3);
 
-        assertEquals(result, "III");
+        assertEquals("III",result);
+    }
+
+    //------- Test from 7 to 10 -------
+    /**
+     * Test the number 7
+     */
+    @Test
+    public void testNumberSevenConversion()
+    {
+
+        String result = IntegerToRoman.convert(7);
+
+        assertEquals("VII",result);
+    }
+
+    /**
+     * Test the number 8
+     */
+    @Test
+    public void testNumberEightConversion()
+    {
+
+        String result = IntegerToRoman.convert(7);
+
+        assertEquals("VIII",result);
+    }
+
+    /**
+     * Test the number 9
+     */
+    @Test
+    public void testNumberNineConversion()
+    {
+
+        String result = IntegerToRoman.convert(9);
+
+        assertEquals("IX",result);
+    }
+
+    /**
+     * Test the number 10
+     */
+    @Test
+    public void testNumberTenConversion()
+    {
+
+        String result = IntegerToRoman.convert(10);
+
+        assertEquals("X",result);
     }
 }
+
+
+
